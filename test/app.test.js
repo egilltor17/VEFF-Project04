0d8d1c5e8475ab1f28d3ef46dba010c83c0cb16d
egilltor17 <egilltor17@ru.is>
2019-04-02 16:14:15 +0000

    added id validation

--- test/app.test.js
+++ test/app.test.js
@@ -3,7 +3,8 @@ let mongoose = require("mongoose");
 let Station = require('../models/station');
 let Observation = require('../models/observation');
 let server = require('../app');
-var sha256 = require('js-sha256');
+let util = require('../utility/objectIdChecker')
+let sha256 = require('js-sha256');
 
 // These are the actual modules we use
 let chai = require('chai');
@@ -50,13 +51,14 @@ describe('Endpoint tests', () => {
     describe('Regular Endpoint Tests', () => {
         // 1. GET /api/v1/stations
         it("should make a GET request to stations /api/v1/stations", (done) => {
-            chai.request('http:// localhost:3000')
+            chai.request('http://localhost:3000')
                 .get('/api/v1/stations')
                 .end((err, res) => {
                     chai.expect(res).to.have.status(200);
                     chai.expect(res).to.be.json;
                     chai.expect(res).to.have.property('body').an('array');
                     chai.expect(res.body.length).to.be.equal(1);
+                    chai.expect(util.isValidObjectID(String(stationId))).to.be.true;
                     chai.expect(res.body[0]).to.have.property('_id').equal(String(stationId));
                     chai.expect(res.body[0]).to.have.property('description').equal('Reykjavik');
                     chai.expect(Object.keys(res.body[0]).length).to.equal(2);
@@ -66,12 +68,13 @@ describe('Endpoint tests', () => {
         
         // 2. GET /api/v1/stations/:id
         it("should make a GET request to /api/v1/stations/:id", (done) => {
-            chai.request('http:// localhost:3000')
+            chai.request('http://localhost:3000')
                 .get('/api/v1/stations/' + String(stationId))
                 .end((err, res) => {
                     chai.expect(res).to.have.status(200);
                     chai.expect(res).to.be.json;
                     chai.expect(res).to.have.property('body').an('object');
+                    chai.expect(util.isValidObjectID(String(stationId))).to.be.true;
                     chai.expect(res.body).to.have.property('_id').equal(String(stationId));
                     chai.expect(res.body).to.have.property('description').equal('Reykjavik');
                     chai.expect(res.body).to.have.property('lat').equal(64.1275);
@@ -85,7 +88,7 @@ describe('Endpoint tests', () => {
             // 3. POST /api/v1/stations
             it("should make a POST request to /api/v1/stations", (done) => {
                 let newStation = { description : "Akureyri", lat : 65.6826, lon : 18.0907 }
-                chai.request('http:// localhost:3000')
+                chai.request('http://localhost:3000')
                 .post('/api/v1/stations/')
                 .set("content-type", "application/json")
                 .send(newStation)
@@ -94,6 +97,7 @@ describe('Endpoint tests', () => {
                     chai.expect(res).to.be.json;
                     chai.expect(res).to.have.property('body').an('object');
                     chai.expect(res.body).to.have.property('_id');
+                    chai.expect(util.isValidObjectID(String(res.body._id))).to.be.true;
                     chai.expect(res.body).to.have.property('description').equal("Akureyri");
                     chai.expect(res.body).to.have.property('lat').equal(65.6826);
                     chai.expect(res.body).to.have.property('lon').equal(18.0907);
@@ -104,7 +108,7 @@ describe('Endpoint tests', () => {
 
         // 4. GET /api/v1/stations/:stationId/observations
         it("should make a GET request to /api/v1/stations/:stationId/observations", (done) => {
-            chai.request('http:// localhost:3000')
+            chai.request('http://localhost:3000')
                 .get('/api/v1/stations/' + String(stationId) + '/observations')
                 .end((err, res) => {
                     chai.expect(res).to.have.status(200);
@@ -114,6 +118,7 @@ describe('Endpoint tests', () => {
                     // We where not sure if we could skip the following
                     // tests since they are tested in 5.
                     chai.expect(res.body[0]).to.have.property('_id');
+                    chai.expect(util.isValidObjectID(String(res.body[0]._id))).to.be.true;
                     chai.expect(res.body[0]).to.have.property('temp').equal(2);
                     chai.expect(res.body[0]).to.have.property('windSpeed').equal(30.5);
                     chai.expect(res.body[0]).to.have.property('windDir').equal("ne");
@@ -126,12 +131,14 @@ describe('Endpoint tests', () => {
         
         // 5. GET /api/v1/stations/:stationId/observations/:obsId
         it("should make a GET request to /api/v1/stations/:stationId/observations/:obsId", (done) => {
-            chai.request('http:// localhost:3000')
+            chai.request('http://localhost:3000')
                 .get('/api/v1/stations/' + String(stationId) + '/observations/' + String(observationId))
                 .end((err, res) => {
                     chai.expect(res).to.have.status(200);
                     chai.expect(res).to.be.json;
                     chai.expect(res).to.have.property('body').an('object');
+                    chai.expect(util.isValidObjectID(String(stationId))).to.be.true;
+                    chai.expect(util.isValidObjectID(String(observationId))).to.be.true;
                     chai.expect(res.body).to.have.property('_id').equal(String(observationId));
                     chai.expect(res.body).to.have.property('temp').equal(2);
                     chai.expect(res.body).to.have.property('windSpeed').equal(30.5);
@@ -146,7 +153,7 @@ describe('Endpoint tests', () => {
         // 6.A POST /api/v1/stations/:stationId/observations
         it("should make a POST request to /api/v1/stations/:stationId/observations", (done) => {
             let newObservation = { temp: 5, windSpeed: 23, windDir: "s", hum: 10.1, prec: 42 }
-            chai.request('http:// localhost:3000')
+            chai.request('http://localhost:3000')
                 .post('/api/v1/stations/' + String(stationId) + '/observations/')
                 .set('Content-Type','application/json')
                 .send(newObservation)
@@ -155,6 +162,7 @@ describe('Endpoint tests', () => {
                     chai.expect(res).to.be.json;
                     chai.expect(res).to.have.property('body').an('object');
                     chai.expect(res.body).to.have.property('_id');
+                    chai.expect(util.isValidObjectID(String(res.body._id))).to.be.true;
                     chai.expect(res.body).to.have.property('temp').equal(5);
                     chai.expect(res.body).to.have.property('windSpeed').equal(23);
                     chai.expect(res.body).to.have.property('windDir').equal("s");
@@ -169,7 +177,7 @@ describe('Endpoint tests', () => {
         it("should make a Bad POST request to /api/v1/stations/:stationId/observations", (done) => {
             console.log = function() {}                  // suppress loged errors  
             let newObservation = { temp: 5, windSpeed: 23, /* windDir missing */ hum: 34, prec: 42 }
-            chai.request('http:// localhost:3000')
+            chai.request('http://localhost:3000')
                 .post('/api/v1/stations/' + String(stationId) + '/observations')
                 .set('Content-Type','application/json')
                 .send(newObservation)
@@ -188,7 +196,7 @@ describe('Endpoint tests', () => {
         it("should make a Bad Post request to /api/v1/stations/:stationId/observations with invalid humidity", (done)=> {
             console.log = function() {}                 // suppress loged errors  
             let newObservation = { temp: 5, windSpeed: 23, windDir: "s", hum: 100.1, prec: 42 }
-            chai.request('http:// localhost:3000')
+            chai.request('http://localhost:3000')
                 .post('/api/v1/stations/' + String(stationId) + '/observations')
                 .set('Content-Type','application/json')
                 .send(newObservation)
@@ -205,12 +213,14 @@ describe('Endpoint tests', () => {
         
         // 7. DELETE /api/v1/stations/:stationId/observations/:obsId
         it("should make a DELETE request to /api/v1/stations/:stationId/observations/:obsId", (done) => {
-            chai.request('http:// localhost:3000')
+            chai.request('http://localhost:3000')
                 .delete(`/api/v1/stations/${stationId}/observations/${observationId}`)
                 .end((err, res) => {
                     chai.expect(res).to.have.status(200);
                     chai.expect(res).to.be.json;
                     chai.expect(res).to.have.property('body').an('object');
+                    chai.expect(util.isValidObjectID(String(stationId))).to.be.true;
+                    chai.expect(util.isValidObjectID(String(observationId))).to.be.true;
                     chai.expect(res.body).to.have.property('_id').equal(String(observationId));
                     chai.expect(res.body).to.have.property('temp').equal(2);
                     chai.expect(res.body).to.have.property('windSpeed').equal(30.5);
@@ -224,7 +234,7 @@ describe('Endpoint tests', () => {
 
         // 10. Invalid verb to /api/v1/stations
         it("should return 405 if invalid verb sent to /api/v1/stations", (done) => {
-            chai.request('http:// localhost:3000')
+            chai.request('http://localhost:3000')
                 .put('/api/v1/stations')
                 .end((err, res) => {
                     chai.expect(res).to.have.status(405);
@@ -241,7 +251,7 @@ describe('Endpoint tests', () => {
         // 8.A DELETE /api/v1/stations/:id
         it("should deny a DELETE request to /api/v1/stations/:id without authentication", (done) => {
             // No authentication.
-            chai.request('http:// localhost:3000')
+            chai.request('http://localhost:3000')
                 .delete('/api/v1/stations/' + stationId)
                 .end((err, res) => {
                     chai.expect(res).to.have.status(401);
@@ -256,12 +266,13 @@ describe('Endpoint tests', () => {
             // 8.B DELETE /api/v1/stations/:id
             it("should make a DELETE request to /api/v1/stations/:id with authentication", (done) => {
                 // With correct authentication.
-                chai.request('http:// localhost:3000')
+                chai.request('http://localhost:3000')
                 .delete('/api/v1/stations/' + stationId)
                 .set('authorization', sha256.hmac('mysecret', 'DELETE /api/v1/stations/' + stationId))
                 .end((err, res) => {
                     chai.expect(res).to.have.status(200);
                     chai.expect(res).to.be.json;
+                    chai.expect(util.isValidObjectID(String(stationId))).to.be.true;
                     chai.expect(res.body).to.have.property('_id').equal(String(stationId));
                     chai.expect(res.body).to.have.property('description').equal('Reykjavik');
                     chai.expect(res.body).to.have.property('lat').equal(64.1275);
@@ -270,6 +281,7 @@ describe('Endpoint tests', () => {
                     chai.expect(Object.keys(res.body).length).to.equal(5);
                     chai.expect(res.body.observations.length).to.equal(1);
                     chai.expect(res.body.observations[0]).to.have.property('_id');
+                    chai.expect(util.isValidObjectID(String(res.body.observations[0]._id))).to.be.true;
                     chai.expect(res.body.observations[0]).to.have.property('temp').equal(2);
                     chai.expect(res.body.observations[0]).to.have.property('windSpeed').equal(30.5);
                     chai.expect(res.body.observations[0]).to.have.property('windDir').equal("ne");
@@ -282,7 +294,7 @@ describe('Endpoint tests', () => {
 
         // 9. Injection attact! ?description={ $ne: "Akureyri" }
         it('should return "Reykjavik" with GET /api/v1/stations?description={ $ne: "Akureyri" }', (done) => {
-            chai.request('http:// localhost:3000')
+            chai.request('http://localhost:3000')
                 .get('/api/v1/stations')
                 .query({ description: { $ne: "Akureyri" } })
                 .end((err, res) => {
@@ -291,6 +303,7 @@ describe('Endpoint tests', () => {
                     chai.expect(res).to.have.property('body').an('array');
                     chai.expect(res.body.length).to.equal(1);
                     chai.expect(res.body[0]).to.have.property('_id');
+                    chai.expect(util.isValidObjectID(String(res.body[0]._id))).to.be.true;
                     chai.expect(res.body[0]).to.have.property('description').equal('Reykjavik');
                     chai.expect(Object.keys(res.body[0]).length).to.equal(2);
                     done();
