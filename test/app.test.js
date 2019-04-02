3b70e31bce75435a4fceefdd09a39bcddacf2050
egilltor17 <egilltor17@ru.is>
2019-04-02 15:58:49 +0000

    minor adjustments

--- test/app.test.js
+++ test/app.test.js
@@ -1,11 +1,11 @@
-//These four imports are required for setup
+// These four imports are required for setup
 let mongoose = require("mongoose");
 let Station = require('../models/station');
 let Observation = require('../models/observation');
 let server = require('../app');
 var sha256 = require('js-sha256');
 
-//These are the actual modules we use
+// These are the actual modules we use
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 chai.use(chaiHttp);
@@ -14,19 +14,19 @@ describe('Endpoint tests', () => {
     // We disable console log in tests 6 B and C, and then restore it to suppress errors from observation.js
     let consoleLogStorrage = console.log 
 
-    //###########################
-    //These variables contain the ids of the existing station/observation
-    //That way, you can use them in your tests (e.g., to get all observations for a station)
-    //###########################
+    // ###########################
+    // These variables contain the ids of the existing station/observation
+    // That way, you can use them in your tests (e.g., to get all observations for a station)
+    // ###########################
 
     let stationId = '';
     let observationId = '';
 
-    //###########################
-    //The beforeEach function makes sure that before each test, 
-    //there is exactly one station and one observation (for the existing station).
-    //The ids of both are stored in stationId and observationId
-    //###########################
+    // ###########################
+    // The beforeEach function makes sure that before each test, 
+    // there is exactly one station and one observation (for the existing station).
+    // The ids of both are stored in stationId and observationId
+    // ###########################
     beforeEach((done) => {
         let station = new Station({ description: "Reykjavik", lat: 64.1275, lon: 21.9028 });
         let observation = new Observation({ stationId: station._id, temp: 2.0, windSpeed: 30.5, windDir: "ne", hum: 20.5, prec: 0.0 });
@@ -44,13 +44,13 @@ describe('Endpoint tests', () => {
         });
     });
     
-    //###########################
-    //Write your tests below here
-    //###########################
+    // ###########################
+    // Write your tests below here
+    // ###########################
     describe('Regular Endpoint Tests', () => {
         // 1. GET /api/v1/stations
         it("should make a GET request to stations /api/v1/stations", (done) => {
-            chai.request('http://localhost:3000')
+            chai.request('http:// localhost:3000')
                 .get('/api/v1/stations')
                 .end((err, res) => {
                     chai.expect(res).to.have.status(200);
@@ -66,7 +66,7 @@ describe('Endpoint tests', () => {
         
         // 2. GET /api/v1/stations/:id
         it("should make a GET request to /api/v1/stations/:id", (done) => {
-            chai.request('http://localhost:3000')
+            chai.request('http:// localhost:3000')
                 .get('/api/v1/stations/' + String(stationId))
                 .end((err, res) => {
                     chai.expect(res).to.have.status(200);
@@ -85,7 +85,7 @@ describe('Endpoint tests', () => {
             // 3. POST /api/v1/stations
             it("should make a POST request to /api/v1/stations", (done) => {
                 let newStation = { description : "Akureyri", lat : 65.6826, lon : 18.0907 }
-                chai.request('http://localhost:3000')
+                chai.request('http:// localhost:3000')
                 .post('/api/v1/stations/')
                 .set("content-type", "application/json")
                 .send(newStation)
@@ -104,7 +104,7 @@ describe('Endpoint tests', () => {
 
         // 4. GET /api/v1/stations/:stationId/observations
         it("should make a GET request to /api/v1/stations/:stationId/observations", (done) => {
-            chai.request('http://localhost:3000')
+            chai.request('http:// localhost:3000')
                 .get('/api/v1/stations/' + String(stationId) + '/observations')
                 .end((err, res) => {
                     chai.expect(res).to.have.status(200);
@@ -126,7 +126,7 @@ describe('Endpoint tests', () => {
         
         // 5. GET /api/v1/stations/:stationId/observations/:obsId
         it("should make a GET request to /api/v1/stations/:stationId/observations/:obsId", (done) => {
-            chai.request('http://localhost:3000')
+            chai.request('http:// localhost:3000')
                 .get('/api/v1/stations/' + String(stationId) + '/observations/' + String(observationId))
                 .end((err, res) => {
                     chai.expect(res).to.have.status(200);
@@ -146,7 +146,7 @@ describe('Endpoint tests', () => {
         // 6.A POST /api/v1/stations/:stationId/observations
         it("should make a POST request to /api/v1/stations/:stationId/observations", (done) => {
             let newObservation = { temp: 5, windSpeed: 23, windDir: "s", hum: 10.1, prec: 42 }
-            chai.request('http://localhost:3000')
+            chai.request('http:// localhost:3000')
                 .post('/api/v1/stations/' + String(stationId) + '/observations/')
                 .set('Content-Type','application/json')
                 .send(newObservation)
@@ -169,7 +169,7 @@ describe('Endpoint tests', () => {
         it("should make a Bad POST request to /api/v1/stations/:stationId/observations", (done) => {
             console.log = function() {}                  // suppress loged errors  
             let newObservation = { temp: 5, windSpeed: 23, /* windDir missing */ hum: 34, prec: 42 }
-            chai.request('http://localhost:3000')
+            chai.request('http:// localhost:3000')
                 .post('/api/v1/stations/' + String(stationId) + '/observations')
                 .set('Content-Type','application/json')
                 .send(newObservation)
@@ -188,9 +188,9 @@ describe('Endpoint tests', () => {
         it("should make a Bad Post request to /api/v1/stations/:stationId/observations with invalid humidity", (done)=> {
             console.log = function() {}                 // suppress loged errors  
             let newObservation = { temp: 5, windSpeed: 23, windDir: "s", hum: 100.1, prec: 42 }
-            chai.request('http://localhost:3000')
-            .post('/api/v1/stations/' + String(stationId) + '/observations')
-            .set('Content-Type','application/json')
+            chai.request('http:// localhost:3000')
+                .post('/api/v1/stations/' + String(stationId) + '/observations')
+                .set('Content-Type','application/json')
                 .send(newObservation)
                 .end((err, res)=> {
                     chai.expect(res).to.have.status(400);
@@ -205,7 +205,7 @@ describe('Endpoint tests', () => {
         
         // 7. DELETE /api/v1/stations/:stationId/observations/:obsId
         it("should make a DELETE request to /api/v1/stations/:stationId/observations/:obsId", (done) => {
-            chai.request('http://localhost:3000')
+            chai.request('http:// localhost:3000')
                 .delete(`/api/v1/stations/${stationId}/observations/${observationId}`)
                 .end((err, res) => {
                     chai.expect(res).to.have.status(200);
@@ -224,7 +224,7 @@ describe('Endpoint tests', () => {
 
         // 10. Invalid verb to /api/v1/stations
         it("should return 405 if invalid verb sent to /api/v1/stations", (done) => {
-            chai.request('http://localhost:3000')
+            chai.request('http:// localhost:3000')
                 .put('/api/v1/stations')
                 .end((err, res) => {
                     chai.expect(res).to.have.status(405);
@@ -241,7 +241,7 @@ describe('Endpoint tests', () => {
         // 8.A DELETE /api/v1/stations/:id
         it("should deny a DELETE request to /api/v1/stations/:id without authentication", (done) => {
             // No authentication.
-            chai.request('http://localhost:3000')
+            chai.request('http:// localhost:3000')
                 .delete('/api/v1/stations/' + stationId)
                 .end((err, res) => {
                     chai.expect(res).to.have.status(401);
@@ -256,7 +256,7 @@ describe('Endpoint tests', () => {
             // 8.B DELETE /api/v1/stations/:id
             it("should make a DELETE request to /api/v1/stations/:id with authentication", (done) => {
                 // With correct authentication.
-                chai.request('http://localhost:3000')
+                chai.request('http:// localhost:3000')
                 .delete('/api/v1/stations/' + stationId)
                 .set('authorization', sha256.hmac('mysecret', 'DELETE /api/v1/stations/' + stationId))
                 .end((err, res) => {
@@ -282,7 +282,7 @@ describe('Endpoint tests', () => {
 
         // 9. Injection attact! ?description={ $ne: "Akureyri" }
         it('should return "Reykjavik" with GET /api/v1/stations?description={ $ne: "Akureyri" }', (done) => {
-            chai.request('http://localhost:3000')
+            chai.request('http:// localhost:3000')
                 .get('/api/v1/stations')
                 .query({ description: { $ne: "Akureyri" } })
                 .end((err, res) => {
